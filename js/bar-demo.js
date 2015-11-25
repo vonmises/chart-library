c722c6b58b51a4117f00f1cab7137c9b8715bdf4
vonmises <vonmises@users.noreply.github.com>
2015-11-25 21:02:16 +0300

    added bar chart class and demo code

---
+++ js/bar-demo.js
@@ -0,0 +1,17 @@
+(function () {
+  var random = Math.random;
+
+  var context = document.getElementById("bar").getContext("2d");
+
+  var graph = new BarChart(context);
+  graph.width = 800;
+  graph.height = 400;
+  graph.maxValue = 30;
+  graph.margin = 2;
+  graph.xAxisLabelArr = ["January", "February", "March", "April", "May",
+  "June", "July", "August", "September",
+  "October", "November", "December"];
+  graph.update([random() * 30, random() * 30, random() * 30, random() * 30,
+  random() * 30, random() * 30, random() * 30, random() * 30,
+  random() * 30, random() * 30, random() * 30, random() * 30,]);
+}());
