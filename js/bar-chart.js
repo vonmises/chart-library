5c6a4019ba73c8f24818eec88b5b099abd9930a3
vonmises <vonmises@users.noreply.github.com>
2015-11-26 10:36:49 +0300

    removed bar and text shadows, and redundant comments

--- js/bar-chart.js
+++ js/bar-chart.js
@@ -1,14 +1,10 @@
-function BarChart(context) {
-
-  // Private properties and methods
-  
+function BarChart(context) {  
   var that = this;
   var startArr;
   var endArr;
   
-  // Draw method updates the canvas with the current display
+  // draw method updates the canvas with the current display
   var draw = function (arr) {
-
     var numOfBars = arr.length;
     var barWidth;
     var barHeight;
@@ -27,16 +23,16 @@ function BarChart(context) {
     context.fillStyle = that.backgroundColor;
     context.fillRect(0, 0, that.width, that.height);
 
-    // If x axis labels exist then make room
+    // if x axis labels exist then make room
     if (that.xAxisLabelArr.length) {
       graphAreaHeight -= 40;
     }
 
-    // Calculate dimensions of the bar
+    // bar dimensions
     barWidth = graphAreaWidth / numOfBars - that.margin * 2;
     maxBarHeight = graphAreaHeight - 25;
 
-    // Determine the largest value in the bar array
+    // largest value in the bar array
     var largestValue = 0;
     for (i = 0; i < arr.length; i += 1) {
       if (arr[i] > largestValue) {
@@ -44,9 +40,9 @@ function BarChart(context) {
       }
     }
     
-    // For each bar
+    // for each bar...
     for (i = 0; i < arr.length; i += 1) {
-      // Set the ratio of current bar compared to the maximum
+      // set the ratio of current bar compared to the maximum
       if (that.maxValue) {
         ratio = arr[i] / that.maxValue;
       } else {
@@ -55,35 +51,28 @@ function BarChart(context) {
     
       barHeight = ratio * maxBarHeight;
 
-      // Turn on shadow
-      context.shadowOffsetX = 2;
-      context.shadowOffsetY = 2;
-      context.shadowBlur = 2;
-      context.shadowColor = "#999";
-
-      // Draw bar background
+      // bar background
       context.fillStyle = "#333";      
       context.fillRect(that.margin + i * that.width / numOfBars,
         graphAreaHeight - barHeight,
         barWidth,
         barHeight);
 
-      // Draw bar color if it is large enough to be visible
+      // draw bar colour if it is large enough to be visible
       if (barHeight > border * 2) {
-        // Create gradient
         gradient = context.createLinearGradient(0, 0, 0, graphAreaHeight);
         gradient.addColorStop(1-ratio, that.colors[i % that.colors.length]);
         gradient.addColorStop(1, "#ffffff");
 
         context.fillStyle = gradient;
-        // Fill rectangle with gradient
+
         context.fillRect(that.margin + i * that.width / numOfBars + border,
           graphAreaHeight - barHeight + border,
           barWidth - border * 2,
           barHeight - border * 2);
       }
 
-      // Write bar value
+      // write bar value
       context.fillStyle = "#333";
       context.font = "bold 12px sans-serif";
       context.textAlign = "center";
@@ -92,7 +81,7 @@ function BarChart(context) {
       i * that.width / numOfBars + (that.width / numOfBars) / 2,
       graphAreaHeight - barHeight - 10);
 
-      // Draw bar label if it exists
+      // draw bar label if it exists
       if (that.xAxisLabelArr[i]) {      
           context.fillStyle = "#333";
           context.font = "bold 12px sans-serif";
@@ -105,7 +94,7 @@ function BarChart(context) {
       }
     };
 
-  // Public properties and methods
+  // public properties and methods
 
   this.maxValue;
   this.margin = 5;
@@ -117,17 +106,17 @@ function BarChart(context) {
   this.animationInterval = 100;
   this.animationSteps = 10;
 
-  // Update method sets the end bar array and starts the animation
+  // update method sets the end bar array and starts the animation
   this.update = function (newArr) {
 
-    // If length of target and current array is different 
+    // if length of target and current array is different 
     if (that.curArr.length !== newArr.length) {
       that.curArr = newArr;
       draw(newArr);
     } else {
-        // Set the starting array to the current array
+        // set the starting array to the current array
         startArr = that.curArr;
-        // Set the target array to the new array
+        // set the target array to the new array
         endArr = newArr;
     }
   }; 
