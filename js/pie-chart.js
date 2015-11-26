720fafa9767bff9d6e7eed6d20618d0d15087d4f
vonmises <vonmises@users.noreply.github.com>
2015-11-26 17:28:19 +0300

    stored city and colour information for use in legends or other places

--- js/pie-chart.js
+++ js/pie-chart.js
@@ -3,26 +3,29 @@ function createPieChart(canvas, context, data){
   var center = [canvas.width / 2, canvas.height / 2];
   var radius = Math.min(canvas.width, canvas.height) / 2;
   var previous_position = 0, next_position = 0, total = 0;
+  // numerical index for data[] and colours[], string i for pie_data[]
+  var index = 0;
 
   for (var i in data) { total += data[i]; }
 
   var pie_data = [];
-  var keys = Object.keys(data);
+  var cities = Object.keys(data);
 
   for (var i in data) {
     pie_data[i] = [];
-    pie_data[i]["key"] = keys[i];
+    pie_data[i]["city"] = cities[index];
+    pie_data[i]["colour"] = colours[index];
     pie_data[i]["value"] = data[i];
     pie_data[i]["start_angle"] = 2 * Math.PI * previous_position;
     pie_data[i]["end_angle"] = 2 * Math.PI * (
     previous_position + (data[i] / total));
     previous_position += data[i] / total;
+    index++;
   }
 
+  index = 0;
   previous_position = 0;
 
-  // numerical index for data[] and colours[], string i for pie_data[]
-  var index = 0;
   for (var i in data){
     next_position = previous_position + (Math.PI * 2 * (data[index] / total));
     context.beginPath();
